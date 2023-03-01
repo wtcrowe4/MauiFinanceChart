81b191490b88ed0ad83e35c86787e40ba9c0c8e5
Thomas Crowe <wcrowe@g.clemson.edu>
2023-02-28 23:18:50 -0500

    Stocks http request working

--- MauiFinance/MainPage.xaml.cs
+++ MauiFinance/MainPage.xaml.cs
@@ -1,24 +1,58 @@
-﻿namespace MauiFinance;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MauiFinance;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
+
+    
 	public MainPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
+        GetStocks();
+        
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    
+    public ICommand GetStocksCommand { get; }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+   
+    
+    // Http request for stocks data
+    private static async void GetStocks()
+    {
+        var api_key = Environment.GetEnvironmentVariable("RAPIDAPI_KEY");
+        Debug.WriteLine(api_key);
+        var searchTerm = "microsoft";
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://yh-finance.p.rapidapi.com/auto-complete?q={searchTerm}"),
+            Headers =
+            {
+                { "X-RapidAPI-Key", "api_key" },
+                { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+            },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Debug.WriteLine(body);
+            Debug.WriteLine(api_key);
+        }
+
+
+    }
+
+
+
+
+
 }
 
